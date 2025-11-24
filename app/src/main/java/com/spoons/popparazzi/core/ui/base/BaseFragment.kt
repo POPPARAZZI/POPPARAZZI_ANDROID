d1a958f7cf8198e0d22646c920bce8bb0dd63af8
hack <skek933@gmail.com>
2025-11-24 23:06:25 +0900

    기본 테마 설정, 스플래시

--- app/src/main/java/com/spoons/popparazzi/core/ui/base/BaseFragment.kt
+++ app/src/main/java/com/spoons/popparazzi/core/ui/base/BaseFragment.kt
@@ -2,7 +2,9 @@ package com.spoons.popparazzi.core.ui.base
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.spoons.popparazzi.R
 import com.spoons.popparazzi.core.ui.message.CallbackMessage
 import com.spoons.popparazzi.core.util.LogUtil
 
@@ -10,6 +12,16 @@ abstract class BaseFragment: Fragment() {
     protected open fun bindUi() = Unit
     protected open fun regListener() = Unit
     protected open fun regObserve() = Unit
+
+    open val containerColor: Int = R.color.color_container
+
+    override fun onResume() {
+        super.onResume()
+        requireActivity().window.decorView.setOnApplyWindowInsetsListener { v, i ->
+            v.setBackgroundColor(ContextCompat.getColor(requireContext(), containerColor))
+            i
+        }
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
--- app/src/main/java/com/spoons/popparazzi/feature/splash/SplashFragment.kt
+++ app/src/main/java/com/spoons/popparazzi/feature/splash/SplashFragment.kt
@@ -1,4 +1,9 @@
 package com.spoons.popparazzi.feature.splash
 
-class SplashFragment {
+import com.spoons.popparazzi.R
+import com.spoons.popparazzi.core.ui.base.BaseBindFragment
+import com.spoons.popparazzi.databinding.FragmentSplashBinding
+
+class SplashFragment: BaseBindFragment<FragmentSplashBinding>(FragmentSplashBinding::inflate) {
+    override val containerColor: Int = R.color.color_primary
 }
