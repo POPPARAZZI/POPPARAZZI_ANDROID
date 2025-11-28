7438657c44c31e7cc76131006eab53c3172b6517
hack <skek933@gmail.com>
2025-11-28 21:57:37 +0900

    naver map sdk

--- app/src/main/java/com/spoons/popparazzi/core/App.kt
+++ app/src/main/java/com/spoons/popparazzi/core/App.kt
@@ -1,6 +1,18 @@
 package com.spoons.popparazzi.core
 
 import android.app.Application
+import com.naver.maps.map.NaverMapSdk
+import com.spoons.popparazzi.BuildConfig
 
 class App: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        initNaverMap()
+    }
+
+    private fun initNaverMap() {
+        NaverMapSdk.getInstance(this).client = NaverMapSdk.NcpKeyClient(BuildConfig.NAVER_CLIENT_ID)
+    }
 }
