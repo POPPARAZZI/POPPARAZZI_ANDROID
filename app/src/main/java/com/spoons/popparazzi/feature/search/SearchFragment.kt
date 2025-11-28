17d6bca9bacefb9192e005f85f9c2ec96ff4f6d9
hack <skek933@gmail.com>
2025-11-29 01:22:24 +0900

    검색 애니메이션

--- app/src/main/java/com/spoons/popparazzi/feature/search/SearchFragment.kt
+++ app/src/main/java/com/spoons/popparazzi/feature/search/SearchFragment.kt
@@ -4,4 +4,16 @@ import com.spoons.popparazzi.core.ui.base.fragment.BaseViewBindingFragment
 import com.spoons.popparazzi.databinding.FragmentSearchBinding
 
 class SearchFragment: BaseViewBindingFragment<FragmentSearchBinding>(FragmentSearchBinding::inflate) {
+
+    override fun regListener() {
+        super.regListener()
+
+        binding.clSearch.setOnClickListener {
+            binding.motion.transitionToStart()
+        }
+
+        binding.clCategory.setOnClickListener {
+            binding.motion.transitionToEnd()
+        }
+    }
 }
