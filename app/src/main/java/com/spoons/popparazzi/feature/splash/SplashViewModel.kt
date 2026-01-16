81ad8017dac1ad6781a4ab5df6d0588040a73cca
hack <skek933@bookswage.com>
2026-01-16 17:20:10 +0900

    누락 파일

--- app/src/main/java/com/spoons/popparazzi/feature/splash/SplashViewModel.kt
+++ app/src/main/java/com/spoons/popparazzi/feature/splash/SplashViewModel.kt
@@ -1,9 +1,10 @@
 package com.spoons.popparazzi.feature.splash
 
+import com.spoons.popparazzi.core.domain.usecase.user.GetUserUseCase
 import com.spoons.popparazzi.core.ui.base.viewmodel.BaseViewmodel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class SplashViewModel @Inject constructor(): BaseViewmodel() {
+class SplashViewModel @Inject constructor(private val getUserUseCase: GetUserUseCase): BaseViewmodel() {
 }
